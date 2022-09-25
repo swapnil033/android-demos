08972d6eda080e611efb0b26cfc4c6818e9cf21d
swapnil033 <swapnildev33@gmail.com>
2022-09-25 14:33:14 +0530

    Data Binding

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myandroiddemos
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.myandroiddemos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.submitButton.setOnClickListener {
+            displayGreeting()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayGreeting() {
+        binding.apply {
+            greetingTextView.text = "Hello! " + nameEditText.text
+        }
+
     }
 }
