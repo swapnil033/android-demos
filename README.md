9605a805719ab79ffde826751835c4cceacc4f67
swapnil033 <swapnildev33@gmail.com>
2022-09-30 17:23:40 +0530

    coding challenge

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# android-demos
+# Data Binding Coding Challenge
+
+This project has a button and progress bar
+
+progress bar hide and visible on click of the button
--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myandroiddemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.myandroiddemos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.controlButton.setOnClickListener {
+            binding.progressBar.visibility = if (binding.progressBar.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+            binding.controlButton.text = if (binding.progressBar.visibility == View.VISIBLE) "Stop" else "Start"
+        }
+
     }
 }
