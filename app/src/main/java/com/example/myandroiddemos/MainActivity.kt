5e41ba06675c692f71ac10d53979197bb13d74e9
swapnil033 <swapnildev33@gmail.com>
2022-10-01 00:28:33 +0530

    live data example

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myandroiddemos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import com.example.myandroiddemos.databinding.ActivityMainBinding
@@ -18,11 +19,13 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory)[MainActivityViewModel::class.java]
-        binding.resultTextView.text = viewModel.getTotal().toString()
+
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTextView.text = it.toString()
+        })
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text = viewModel.getTotal().toString()
 
         }
 
--- app/src/main/java/com/example/myandroiddemos/MainActivityViewModel.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivityViewModel.kt
@@ -6,18 +6,16 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal : Int) : ViewModel() {
 
-    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData : LiveData<Int>
+        get() = total
 
     init {
-        total = startingTotal
-    }
-
-    fun getTotal():Int{
-        return total
+        total.value = startingTotal
     }
 
     fun setTotal(input:Int){
-        total +=input
+        total.value =(total.value)?.plus(input)
     }
 
 }
