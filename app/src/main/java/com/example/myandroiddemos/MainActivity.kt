6fa0006fb8774e7170b688e76060a17885a57180
swapnil033 <swapnildev33@gmail.com>
2022-10-01 00:47:43 +0530

    viewModel with data binding

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -15,13 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        binding.viewModel = viewModel
 
         viewModel.count.observe(this, Observer {
             binding.countText.text = it.toString()
         })
-
-        binding.button.setOnClickListener {
-            viewModel.getUpdatedCount()
-        }
     }
 }
