0ff57c3373dfcd44976dd70a70bfe4fde1834d33
swapnil033 <swapnildev33@gmail.com>
2022-10-04 22:03:33 +0530

    coding challenge

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myandroiddemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.myandroiddemos.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.i("TAG", "onCreate: ${this.toString()}")
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.e("TAG", "onCreate: ${this.toString()}")
+        }
+
     }
 }
