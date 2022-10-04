653df0fcf8b2c7bf90b78c53c79757f755aa93fc
swapnil033 <swapnildev33@gmail.com>
2022-10-04 21:38:51 +0530

    kotlin coroutine sample

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.myandroiddemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.myandroiddemos.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.
+        setContentView(this, R.layout.activity_main)
+
+        binding.btnCount.setOnClickListener {
+            binding.tvCount.text = count++.toString()
+        }
+        binding.btnDownloadUserData.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+        }
+    }
+
+    private fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+        }
     }
 }
