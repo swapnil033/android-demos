238b2891b533a83bdaf3050da347cc6e490305df
swapnil033 <swapnildev33@gmail.com>
2022-10-05 19:09:45 +0530

    parallel decomposition commit 3

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.myandroiddemos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -14,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        CoroutineScope(IO).launch {
+        CoroutineScope(Main).launch {
             Log.i("TAG", "transaction started ...")
-            val stock1 = async { getStock1() }
-            val stock2 = async { getStock2() }
+            val stock1 = async(IO) { getStock1() }
+            val stock2 = async(IO) { getStock2() }
             val total = stock1.await() + stock2.await()
+            Toast.makeText(applicationContext, "total : $total", Toast.LENGTH_SHORT).show()
             Log.i("TAG", "onCreate: $total")
         }
 
