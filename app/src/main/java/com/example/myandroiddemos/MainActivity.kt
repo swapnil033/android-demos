5212b0c56d5232f11910a5f5fa025983a0bce0fd
swapnil033 <swapnildev33@gmail.com>
2022-10-05 19:06:08 +0530

    linear decomposition commit 1

--- app/src/main/java/com/example/myandroiddemos/MainActivity.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.myandroiddemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(IO).launch {
+            Log.i("TAG", "transaction started ...")
+            val stock1 = getStock1()
+            val stock2 = getStock2()
+            val total = stock1 + stock2
+            Log.i("TAG", "onCreate: $total")
+        }
+
     }
+
+    private suspend fun getStock1() : Int {
+        delay(10000)
+        Log.i("TAG", "getStock1: getStock 1")
+        return 4000
+    }
+
+    private suspend fun getStock2() : Int {
+        delay(8000)
+        Log.i("TAG", "getStock2: getStock 2")
+        return 6000
+    }
+
 }
