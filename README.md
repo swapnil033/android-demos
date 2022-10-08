e4d61e35578047c62e9393b941697361f2562de4
swapnil033 <swapnildev33@gmail.com>
2022-10-08 13:20:13 +0530

    created SubscriberDAO.kt Dao class

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## 1st commit
 create [Subscriber.kt](app/src/main/java/com/example/myandroiddemos/db/Subscriber.kt) entity class
+
+## 2nd commit
+create [SubscriberDAO.kt](app/src/main/java/com/example/myandroiddemos/db/SubscriberDAO.kt) entity class
---
+++ app/src/main/java/com/example/myandroiddemos/db/SubscriberDAO.kt
@@ -0,0 +1,24 @@
+package com.example.myandroiddemos.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber)
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_class")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_class")
+    fun getAllSubscriber() : LiveData<List<Subscriber>>
+
+}
