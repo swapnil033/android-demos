a1cfca0d2016cfbb1cc4607b6b90a43d3a9b6a1f
swapnil033 <swapnildev33@gmail.com>
2022-10-08 12:16:45 +0530

    live data builder

--- app/src/main/java/com/example/myandroiddemos/MainActivity2.kt
+++ app/src/main/java/com/example/myandroiddemos/MainActivity2.kt
@@ -2,15 +2,22 @@ package com.example.myandroiddemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import com.example.myandroiddemos.databinding.ActivityMain2Binding
 import com.example.myandroiddemos.ui.main.MainFragment
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding : ActivityMain2Binding
+    private val TAG = "progressBar"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
@@ -18,7 +25,12 @@ class MainActivity2 : AppCompatActivity() {
         }
 
         lifecycleScope.launch{
-
+            delay(3000)
+            binding.progressBar.visibility = View.VISIBLE
+            Log.d(TAG, "progressBar is VISIBLE")
+            delay(10000)
+            binding.progressBar.visibility = View.GONE
+            Log.d(TAG, "progressBar is GONE")
         }
 
     }
